78f0199b9e99ed33908896ff08eda919a1955585
Campospa <95729370+Campospa@users.noreply.github.com>
2022-10-24 20:20:56 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,16 @@
+stores = {}
+items = {
+	1: {
+		"name": "Chair",
+		"price": 17.99
+	},
+	2: {
+		"name": "Table",
+		"price": 180.50
+	}
+}
+
+'''
+By using a dictioanry instead of a list nowe no longer need to iterate over the 
+list to fing the items we need, we can now use the index corresponding to the item
+'''
